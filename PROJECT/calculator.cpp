68b7955dbdd3e04af5eea1b95d3d5181213c7a4d
ZAGAM theZAG <zagam0003@gmail.com>
2026-07-05 23:05:19 +0500

    Implement basic calculator structure in calculator.cpp
    Added basic structure for a calculator program with functions for addition, subtraction, and multiplication.

--- PROJECT/calculator.cpp
+++ PROJECT/calculator.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+// Each person will perform those function which will be assigned to them that's it
+
+void addition();
+void subtraction();
+void multiplication();
+
+int main()
+{
+    cout << "Calculator Program\n";
+
+    addition();
+    subtraction();
+    multiplication();
+
+    return 0;
+}
